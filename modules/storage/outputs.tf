cde0f24e51990d43b6887466e587c56e41ebe2ac
ErikNgigi <ericmosesngigi@gmail.com>
2024-09-12 20:36:36 +0300

    fix(domain): output subdomain url

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -2,3 +2,8 @@ output "hugo_site_endpoint" {
   description = "Website endpoint for the hugo website"
   value       = aws_s3_bucket_website_configuration.hugo_site_configuration.website_endpoint
 }
+
+output "hugo_site_domain" {
+  description = "Website domain for the hugo website"
+  value       = aws_s3_bucket.hugo_site_bucket.bucket
+}
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "hugo_site_endpoint" {
   value = format("http://%s", module.storage_module.hugo_site_endpoint)
 }
 
+output "hugo_site_domain" {
+  value = format("http://%s", module.storage_module.hugo_site_domain)
+}
+
 output "route53_name_servers" {
   value = module.network_module.route53_name_servers
 }
